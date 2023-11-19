ed2ad7ce526b60625a93f22a686aa32d9f31619b
0xffabc <139798156+0xffabc@users.noreply.github.com>
2023-11-19 13:43:54 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-class KMDApi {
+const KMD_INSTANCES = { };
+
+class KMDHook {
+  constructor(type, callback) {
+    KMD_INSTANCES[type] = Object.freeze({
+      type: type,
+      processor: callback
+    });
+  }
+}
+window.KMDApi = class KMDApi {
     // Environment
     KMD_BUILD = "0.1";
     KMD_SCRIPT_URL = "";
@@ -6,6 +16,13 @@ class KMDApi {
     KMD_HOOKS = [];
     KMD_LOCK = [];
     
+    StopLoad() {
+      window.stop();
+      fetch(location.href).then(function DocumentProcessor(inner) {
+        const content = inner.replaceAll(this.KMD_SCRIPT_URL, "");
+        document.write(content);
+      });
+    }
     
     SetScript(script) {
         this.KMD_SCRIPT_URL = script;
@@ -18,7 +35,10 @@ class KMDApi {
         this.KMD_LOCK.push(this.KMD_SCRIPT);
         
         const VirtualMachine = new Function(this.KMD_SCRIPT);
-        VirtualMachine();
+        const isolatedWindow = new(function IsolatedWindowConstructor() { 
+          this.prototype = Window.prototype 
+        });
+        VirtualMachine.bind(isolatedWindow)();
     }
 
     AddHook(type, target, injection) {
@@ -32,24 +52,8 @@ class KMDApi {
     PatchScript() {
         fetch(this.KMD_SCRIPT_URL).then(responce => responce.text()).then(function(script) {
             this.KMD_HOOKS.forEach(function(hookType) {
-                switch (hookType.type) {
-                    case "TEXT_HOOK":
-                        script = script.replaceAll(hookType.target, hookType.injection);
-                        break;
-                    case "PROP_HOOK":
-                        Object.defineProperty(Object.prototype, hookType.target, {
-                            get() {
-                                return hookType.injection;
-                            }, set(value) { }
-                        });
-                        break;
-                    case "SET_HOOK":
-                        Object.prototype[hookType.target] = hookType.injection;
-                        break;
-                    case "GLOBAL_HOOK":
-                        window[hookType.target] = hookType.injection;
-                        break;
-                }
+                const HookProcessor = KMD_INSTANCES[hookType.type].processor;
+                script = HookProcessor(script, hookType.target, hookType.injection);
             });
             this.KMD_SCRIPT = script;
             this.ExecuteScript();
@@ -57,9 +61,81 @@ class KMDApi {
     };
 
     constructor() {
-        if ("GM_info" in window && "KMD_LATEST" in GM_info) {
-            throw new TypeError("KMD Versions Mismatch");
-        }
-    
-                                              }
+      console.log(KMD_INSTANCES);
+    }
 };
+
+const TEXT_HOOK = new KMDHook("TEXT_HOOK", function TextProcessor(script, target, injection) {
+  return script.replaceAll(target, injection);
+});
+
+const PROP_HOOK = new KMDHook("PROP_HOOK", function PropertyProcessor(script, target, injection) {
+  delete Object.prototype[target];
+  
+  Object.defineProperty(Object.prototype, target, {
+    get: injection,
+    set: value => value
+  });
+});
+
+const SET_HOOK = new KMDHook("SET_HOOK", function SetHook(script, target, injection) {
+  Object.defineProperty(Object.prototype, target, {
+    value: injection,
+  });
+});
+
+const GLOBAL_HOOK = new KMDHook("GLOBAL_HOOK", function GlobalHook(script, target, injection) {
+  delete Object.prototype[target];
+  
+  Object.defineProperty(Object.prototype, target, {
+    value: injection,
+    writable: false,
+  });
+});
+
+const HTML_HOOK = new KMDHook("HTML_HOOK", async function HtmlHook(script, targets) {
+  const responce = await fetch(location.href);
+  const html = await responce.text();
+  let replacedHtml = html;
+  for (const key of targets) {
+    const target = key;
+    const injection = target[key];
+    
+    replacedHtml = replacedHtml.replaceAll(target, injection);
+  };
+  
+  document.write(replacedHtml);
+  
+  return script;
+});
+
+const PROP_REMOVE = new KMDHook("PROP_REMOVE", function PropertyRemoval(script, target, injection) {
+  delete Object.prototype[target];
+  
+  return script;
+});
+
+const DEFEND_HOOK = new KMDHook("DEFEND_HOOK", function HookDefender(script, target, injection) {
+  Object.hasOwnPropertyDescriptor = new Proxy(Object.hasOwnPropertyDescriptor, {
+    apply(target, that, args) {
+      return Object.freeze({ });
+    }
+  });
+  
+  return script;
+});
+
+const DEFEND_STACK = new KMDApi("DEFEND_STACK", function StackDefender(script, target, injection) {
+  const filename = (new Error).stack;
+  Error = new Proxy(Error, {
+    construct(target, args) {
+      const errorObj = new target(args);
+      const ffarr = filename.split(".js")[0].split(" ");
+      errorObj.stack = errorObj.replaceAll(ffarr[ffarr.length - 1], "*.js");
+      
+      return errorObj;
+    }
+  });
+  
+  return script;
+});
